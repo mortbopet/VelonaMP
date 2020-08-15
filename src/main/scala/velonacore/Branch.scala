7889225f1a028df870dad45f8d45d730caad9a71
Morten Borup Petersen <morten_bp@live.dk>
2020-08-15 15:06:31 +0200

    Add Branch unit

---
+++ src/main/scala/velonacore/Branch.scala
@@ -0,0 +1,26 @@
+package velonamp.velonacore
+
+import velonamp.common.ISA
+import velonamp.velonacore.CTRL.BR
+
+import chisel3._
+import chisel3.util._
+
+class Branch extends Module {
+  val io = IO(new Bundle {
+    val acc = Input(UInt(ISA.REG_WIDTH.W))
+    val ctrl_br = Input(CTRL.BR())
+    val do_branch = Output(Bool())
+  })
+
+  when(
+    io.ctrl_br === CTRL.BR.br ||
+    (io.ctrl_br === CTRL.BR.brz && io.acc === 0.U) ||
+    (io.ctrl_br === CTRL.BR.brp && io.acc.asSInt >= 0.S) ||
+    (io.ctrl_br === CTRL.BR.brn && io.acc.asSInt < 0.S) ||
+    (io.ctrl_br === CTRL.BR.brnz && io.acc != 0.U)) {
+      io.do_branch := true.B
+  } .otherwise {
+    io.do_branch := false.B
+  }
+}
